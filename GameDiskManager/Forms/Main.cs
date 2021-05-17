98c57c324decb2b40c67c7d74aad299ead9ddcee
Chris Smith <csmith@eccovia.com>
2021-05-17 16:29:54 -0600

    Started implementing data structure

---
+++ GameDiskManager/Forms/Main.cs
@@ -0,0 +1,48 @@
+﻿using GameDiskManager.Types;
+using GameDiskManager.Utility;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GameDiskManager.Forms
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+            InitializeGameList();
+        }
+
+        private void InitializeGameList()
+        {
+            DriveInfo[] allDrives = DriveInfo.GetDrives();
+
+            List<Drive> drives = new List<Drive>();
+
+            foreach (DriveInfo d in allDrives)
+            {
+                Drive drive = new Drive
+                {
+                    Name = d.Name,
+                    VolumeLabel = d.VolumeLabel,
+                    DriveType = d.DriveType,
+                    TotalSize = d.TotalSize,
+                    TotalFreeSpace = d.TotalFreeSpace,
+                    AvailableFreeSpace = d.AvailableFreeSpace,
+                    IsReady = d.IsReady,
+                    Active = true,
+                    Priority = 3,
+                };
+                drives.Add(drive);
+            }
+        }
+    }
+}
---
+++ GameDiskManager/Types/DataStore.cs
@@ -0,0 +1,54 @@
+﻿using GameDiskManager.Types;
+using GameDiskManager.Utility;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameDiskManager
+{
+    public class DataStore
+    {
+        public List<Drive> Drives { get; set; }
+        public List<Game> Games { get; set; }
+        public List<GameMigration> Migrations { get; set; }
+    }
+    public static class Data
+    {
+        public static DataStore Store { get; set; } 
+        public static void InitializeDataStore()
+        {
+            Store = Serializer<DataStore>.LoadFromJSONFile(
+                Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData),
+                "GameDiskManager\\DataStore.json"));
+        }
+
+        public static void UpdateData(List<Drive> drives)
+        {
+            Store.Drives.AddRange(Store.Drives.Except<Drive>(drives).ToList());
+            SaveDataStore();
+        }
+
+        public static void UpdateData(List<Game> games)
+        {
+            Store.Games.AddRange(Store.Games.Except<Game>(games).ToList());
+            SaveDataStore();
+        }
+
+        public static void UpdateData(List<GameMigration> migrations)
+        {
+            Store.Migrations.AddRange(Store.Migrations.Except<GameMigration>(migrations).ToList());
+            SaveDataStore();
+        }
+
+        public static void SaveDataStore()
+        {
+            Serializer<DataStore>.WriteToJSONFile(Store,
+                Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData),
+                "GameDiskManager\\DataStore.json"));
+        }
+
+    }
+}
